8deec5b6bcd9ea6d05c4cbf73fd4f866cc63bb7e
kyeaarr <144277949+kyeaarr@users.noreply.github.com>
2023-09-08 16:16:50 +0300

    Add files via upload

---
+++ practice2/src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+    fun main(args: Array<String>) {
+//task1
+        val age1 = 42.0
+        val age2 = 21.0
+
+//task2
+        val avg1 = (age1+age2)/2
+
+//task3
+
+//COMPLETE
+
+//task4
+        val firstName: String = "кирилл"
+        val lastName: String = "кашкаров"
+
+//task5
+        val fullName: String = firstName +" "+lastName
+
+//task6
+        val myDetails: String = "привет, меня зовут $fullName"
+
+//task7
+        val Trip: Triple <Int, Int, Int> = Triple(9, 8, 2023)
+
+//task8
+        val month = Trip.first
+        val day = Trip.second
+        val year = Trip.third
+
+//task9
+        println(month)
+        println()
+        println(year)
+    }
+}
